2d3729dc5f0a73cf80eecbf4799bfaa7265a903c
nobu1611 <organic.nobu@gmail.com>
2023-05-03 19:50:45 +0900

    password_manager.shにステップ２を追記

--- password_manager.sh
+++ password_manager.sh
@@ -4,7 +4,7 @@ echo "パスワードマネージャーへようこそ！"
 
 while true; do
   # 選択肢の表示
-  echo "次の選択肢から入力してください(Add Password/Get Password/Exit)："
+  echo "次の選択肢から入力してください(Add Password / Get Password / Exit)："
   read choice
 
   # Add Password の場合
@@ -16,8 +16,11 @@ while true; do
     echo "パスワードを入力してください："
     read password
 
-    # 入力された情報をファイルに追記
-    echo "$service_name:$username:$password" >> passwords.txt
+    # 入力された情報を一時的にファイルに書き込む
+    echo "$service_name:$username:$password" > pass.txt
+
+    # ファイルを暗号化する
+    # gpg --batch --yes --passphrase="$password" -c pass.txt
 
     echo "パスワードの追加は成功しました。"
 
@@ -27,7 +30,7 @@ while true; do
     read service_name
 
     # ファイルから該当するサービス名の情報を取得
-    password_info=$(grep "^$service_name:" passwords.txt)
+    password_info=$(grep "^$service_name:" pass.txt)
 
     # サービス名が登録されていなかった場合
     if [ -z "$password_info" ]; then
